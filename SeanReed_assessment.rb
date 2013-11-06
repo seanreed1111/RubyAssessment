884a543f6acc7124abeaa504e5526b1b0da79601
Sean Reed <seanreed1111@gmail.com>
2013-11-06 17:48:33 -0500

    Complete Section 2 - Hashes

--- SeanReed_assessment.rb
+++ SeanReed_assessment.rb
@@ -14,3 +14,26 @@ length_array = array.length
 for num in 0..length_array
 	puts "The index value for Jeff is #{num}." if array[num] == "Jeff" 
 end
+
+###2. Hashes
+
+# instructor = {:name=>"Ashley", :age=27}
+
+# a. Add a new key for location and give it the value "NYC".
+  
+# b. Write a statement to print out all the key/value pairs in the hash
+
+# c. Return the name value from the hash.
+
+# d. Return the key name for the value 27.
+
+instructor = {name: "Ashley", age: 27}
+instructor["location"] = "NYC"
+instructor.each {|key,value| puts "#{key}, #{value}"}
+puts instructor[:name]
+instructor.each {|key,value| puts "The key name for the value 27 is #{key}" if value == 27}
+
+
+
+
+
