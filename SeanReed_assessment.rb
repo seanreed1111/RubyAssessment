b98ee3feb7dd48dd29df89748d8aeda01112459f
Sean Reed <seanreed1111@gmail.com>
2013-11-07 10:41:47 -0500

    Finishes 3a-3c

--- SeanReed_assessment.rb
+++ SeanReed_assessment.rb
@@ -33,6 +33,62 @@ instructor.each {|key,value| puts "#{key}, #{value}"}
 puts instructor[:name]
 instructor.each {|key,value| puts "The key name for the value 27 is #{key}" if value == 27}
 
+# ###3. Nested Structures
+
+#   ```ruby
+# school = { 
+#   :name => "Happy Funtime School",
+#   :location => "NYC",
+#   :instructors => [ 
+#     {:name=>"Blake", :subject=>"being awesome" },
+#     {:name=>"Ashley", :subject=>"being better than blake"},
+#     {:name=>"Jeff", :subject=>"karaoke"}
+#   ],
+#   :students => [ 
+#     {:name => "Marissa", :grade => "B"},
+#     {:name=>"Billy", :grade => "F"},
+#     {:name => "Frank", :grade => "A"},
+#     {:name => "Sophie", :grade => "C"}
+#   ]
+# }
+#   ```
+
+#   a. Add a key to the school hash called "founded_in" and set it to the value 2013. 
+#   b. Add a student to the school's students' array.
+
+#   c. Remove "Billy" from the students' array.
+
+#   d. Add a key to every student in the students array called "semester" and assign it the value "Summer".
+#   e. Change Ashley's subject to "being almost better than Blake"
+#   f. Change Frank's grade from "A" to "F".
+#   g. Return the name of the student with a "B".
+#   h. Return the subject of the instructor "Jeff".
+#   i. Write a statement to print out all the values in the school. ***FLAG
+
+
+ruby_school = { 
+  :name => "Happy Funtime School",
+  :location => "NYC",
+  :instructors => [ 
+    {:name=>"Blake", :subject=>"being awesome" },
+    {:name=>"Ashley", :subject=>"being better than blake"},
+    {:name=>"Jeff", :subject=>"karaoke"}
+  ],
+  :students => [ 
+    {:name => "Marissa", :grade => "B"},
+    {:name=>"Billy", :grade => "F"},
+    {:name => "Frank", :grade => "A"},
+    {:name => "Sophie", :grade => "C"}
+  ]
+}
+
+ruby_school[:founded_in] = 2013
+new_student = {name: "Little Bobby Tables", grade: "A+"} #part a
+ruby_school[:students] << new_student  #part b
+
+puts "Deleting student Billy"
+
+ruby_school[:students].delete_at(1) #part c.  deletes Billy's record
 
 
 
